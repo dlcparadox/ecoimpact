f39c0eb1c6416ad58a496e065df70423be35c180
Joshuah.Codenet <nylanderjoshuah@gmail.com>
2026-03-16 16:14:27 -0300

    Ajuste na Result Screen

--- app/src/main/java/br/com/fiap/ecoimpact/screens/ResultScreen.kt
+++ app/src/main/java/br/com/fiap/ecoimpact/screens/ResultScreen.kt
@@ -1,6 +1,7 @@
+@file:Suppress("UNREACHABLE_CODE")
+
 package br.com.fiap.ecoimpact.screens
 
-import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,14 +11,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,6 +28,7 @@ import br.com.fiap.ecoimpact.R
 import br.com.fiap.ecoimpact.data.HistoryRepository
 import br.com.fiap.ecoimpact.model.ActionItem
 import br.com.fiap.ecoimpact.ui.theme.EcoImpactTheme
+import br.com.fiap.ecoimpact.ui.theme.montserratFamily
 
 @Composable
 fun ResultScreen(
@@ -44,12 +48,12 @@ fun ResultScreen(
     HistoryRepository.addRecord(historyText)
     Box(
         modifier = Modifier
-            .background(Color(0xFF6D5454))
+            .background(MaterialTheme.colorScheme.background)
     ){
         Card(
             shape = RoundedCornerShape(24.dp), // curvatura
             colors = CardDefaults.cardColors(
-                containerColor = Color(0xFFD4B1B1)
+                containerColor = MaterialTheme.colorScheme.primary
             ),
             modifier = Modifier
                 .padding(24.dp)
@@ -58,18 +62,24 @@ fun ResultScreen(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(color = Color(0xFFD4B1B1)),
+                    .background(color = MaterialTheme.colorScheme.secondary),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
                 Text(
-                    text = "Resultado do seu impacto",
+                    text = stringResource(R.string.result_of_your_impact),
                     fontSize = 26.sp,
+                    fontFamily = montserratFamily,
+                    fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
 
                 if (selectedActions.isEmpty()) {
-                    Text(stringResource(R.string.nenhuma_a_o_foi_selecionada))
+                    Text(
+                        stringResource(R.string.nenhuma_a_o_foi_selecionada),
+                        fontFamily = montserratFamily,
+                        fontWeight = FontWeight.ExtraLight,
+                    )
                 } else {
                     selectedActions.forEach {
                         Text("${it.emoji} ${it.name}: ${it.co2} kg CO₂")
@@ -84,22 +94,49 @@ fun ResultScreen(
 
                 Button(
                     onClick = onTipsClick,
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 20.dp)
                 ) {
-                    Text(stringResource(R.string.ver_dicas_sustent_veis))
+                    Text(
+                        stringResource(R.string.ver_dicas_sustent_veis),
+                        fontFamily = montserratFamily,
+                        fontWeight = FontWeight.SemiBold,
+                    )
                 }
 
                 Button(
                     onClick = onHistoryClick,
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 20.dp)
                 ) {
-                    Text(stringResource(R.string.ver_hist_rico))
+                    Text(
+                        stringResource(R.string.ver_hist_rico),
+                        fontFamily = montserratFamily,
+                        fontWeight = FontWeight.SemiBold,
+                    )
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
+@Composable
+fun ResultScreenPreview() {
+    EcoImpactTheme {
+        val mockActions = listOf(
+            ActionItem(stringResource(R.string.dirigir_carro), 2.3, "🚗"),
+        )
+
+        ResultScreen(
+            actions = mockActions,
+            onTipsClick = {},
+            onHistoryClick = {}
+        )
     }
 }
